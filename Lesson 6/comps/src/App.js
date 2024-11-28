3e637bdbf293828fad2d8216117a99192be5d10b
Sauravprasai <sauravprasai2016@gmail.com>
2024-11-27 19:12:33 -0500

    Organizing code

--- Lesson 6/comps/src/App.js
+++ Lesson 6/comps/src/App.js
@@ -1,47 +1,8 @@
-import Button from './Component/Button';
-import { FaMousePointer, FaHome, FaShoppingCart, FaClipboardList, FaStopCircle, FaUserAstronaut } from "react-icons/fa";
+import ButtonPage from "./pages/ButtonPage";
 
 function App() {
   return (
-    <div>
-      <div>
-        <Button>
-          <FaHome/>  
-          Click me!!
-        </Button>
-      </div>
-
-      <div>
-        <Button primary rounded outline>
-          <FaMousePointer/>  
-          Click me!!
-        </Button>
-      </div>
-      <div>
-        <Button danger outline>
-          <FaShoppingCart/>  
-          Buy Now!
-        </Button>
-      </div>
-      <div>
-        <Button warning>
-          <FaClipboardList />  
-          See Deal!
-        </Button>
-      </div>
-      <div>
-        <Button secondary outline>
-          <FaStopCircle />  
-          Hide Ads!
-        </Button>
-      </div>
-      <div>
-        <Button success rounded>
-         <FaUserAstronaut />  
-          Something!
-        </Button>
-      </div>
-    </div>
+    <ButtonPage/>
   );
 }
 
---
+++ Lesson 6/comps/src/pages/ButtonPage.js
@@ -0,0 +1,48 @@
+import Button from "../components/Button";
+import { FaMousePointer, FaHome, FaShoppingCart, FaClipboardList, FaStopCircle, FaUserAstronaut } from "react-icons/fa";
+
+function ButtonPage() {
+  return (
+    <div>
+      <div>
+        <Button>
+          <FaHome/>  
+          Click me!!
+        </Button>
+      </div>
+
+      <div>
+        <Button primary rounded outline>
+          <FaMousePointer/>  
+          Click me!!
+        </Button>
+      </div>
+      <div>
+        <Button danger outline>
+          <FaShoppingCart/>  
+          Buy Now!
+        </Button>
+      </div>
+      <div>
+        <Button warning>
+          <FaClipboardList />  
+          See Deal!
+        </Button>
+      </div>
+      <div>
+        <Button secondary outline>
+          <FaStopCircle />  
+          Hide Ads!
+        </Button>
+      </div>
+      <div>
+        <Button success rounded>
+         <FaUserAstronaut />  
+          Something!
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default ButtonPage;
