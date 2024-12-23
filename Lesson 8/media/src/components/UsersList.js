859be565e92134f05b4edfaeeea67524e52b4413
Sauravprasai <sauravprasai2016@gmail.com>
2024-12-22 23:38:47 -0400

    Lesson 21: Completing user components with all the features.

--- Lesson 8/media/src/components/UsersList.js
+++ Lesson 8/media/src/components/UsersList.js
@@ -1,43 +1,42 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { fetchUsers, addUser } from "../store";
 import Skeleton from "./Skeleton";
 import Button from './Button';
+import { useThunk } from "../hooks/use-thunk";
+import UserListItem from "./UsersListItem";
 
 function UserList(){
-    const dispatch = useDispatch ();
-    const {isLoading, data, error} = useSelector((state) => {
+    const [doFetchUsers, isLoadingUsers, loadingUsersError] = useThunk(fetchUsers);
+    const [doAddUsers, isCreatingUser, creatingUserError] = useThunk(addUser);
+
+    const { data } = useSelector((state) => {
         return state.users;
     })
 
     useEffect(()=>{
-        dispatch(fetchUsers())
-    }, [dispatch]);
+        doFetchUsers();
+    }, [doFetchUsers]);
 
-    const handleUserAdd = () => {
-        dispatch(addUser());
+    const handleUserAdd = () => { doAddUsers(); }
+
+    let content;
+    if( isLoadingUsers ){
+        content =  <Skeleton times={6} className="h-10 w-full"/>
+    }else if( loadingUsersError ){
+        content = <div>Error Fetching data...</div>
+    }else{
+        content = data.map((user)=>{
+            return <UserListItem key={user.id} user={user}/>
+        });
     }
 
-    if( isLoading ){
-        return <Skeleton times={6} className="h-10 w-full"/>
-    }
-    if( error ){
-        return <div>Error Fetching data...</div>
-    }
-    
-    const renderedUsers = data.map((user)=>{
-        return <div key={user.id} className="mb-2 border rounded">
-            <div className="flex p-2 justify-between items-center cursor-pointer">
-                {user.name}
-            </div>
-        </div>
-    })
     return <div>
-        <div className="flex flex-row justify-between m-3">
+        <div className="flex flex-row justify-between items-center m-3">
             <h1 className="m-2 text-xl">Users</h1>
-            <Button onClick={handleUserAdd}> + Add User</Button>
+            <Button loading={isCreatingUser } onClick={handleUserAdd}> + Add User</Button>
         </div>
-        {renderedUsers}
+        {content}
     </div>
 }
 export default UserList;
